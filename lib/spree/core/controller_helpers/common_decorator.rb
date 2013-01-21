3d2a276dde57f121067616080bb282decb7edb81
Brian Quinn <briandquinn@gmail.com>
2013-01-21 14:08:49 +0000

    Ensure session[:currency] is still supported when returning current_currency

--- lib/spree/core/controller_helpers/common_decorator.rb
+++ lib/spree/core/controller_helpers/common_decorator.rb
@@ -1,5 +1,11 @@
 Spree::Core::ControllerHelpers::Common.class_eval do
   def current_currency
-    session[:currency] || Spree::Config[:currency]
+    # ensure session currency is supported
+    #
+    if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
+      session[:currency]
+    else
+      Spree::Config[:currency]
+    end
   end
 end
