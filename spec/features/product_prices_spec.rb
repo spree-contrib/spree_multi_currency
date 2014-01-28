4d28aef50706037a2e9251c54fffd904bbc54753
Peter Berkenbosch <peter@pero-ict.nl>
2014-01-28 22:40:17 +0100

    use new expect syntax add spec for disabled currency selector option.

--- spec/features/product_prices_spec.rb
+++ spec/features/product_prices_spec.rb
@@ -5,7 +5,6 @@ describe "Product with prices in multiple currencies" do
   context "with USD,EUR and GBP as currencies" do
     let!(:product) {create(:product)}
 
-
     before do
       reset_spree_preferences do |config|
         config.supported_currencies = "USD,EUR,GBP"
@@ -18,11 +17,27 @@ describe "Product with prices in multiple currencies" do
 
     it "can switch by currency", :js => true do
       visit spree.product_path(product)
-      page.should have_content("$19.99")
+      expect(page).to have_content("$19.99")
       select "EUR",:from => "currency"
-      page.should have_content("€16.00")
+      expect(page).to have_content("€16.00")
       select "GBP",:from => "currency"
-      page.should have_content("£23.00")
+      expect(page).to have_content("£23.00")
     end
+
+    context "and show_currency_selector is false" do
+      before do
+        reset_spree_preferences do |config|
+          config.supported_currencies = "USD,EUR,GBP"
+          config.allow_currency_change = true
+          config.show_currency_selector = false
+        end
+      end
+
+      it "will not render the currency select" do
+        visit spree.product_path(product)
+        expect(page).to_not have_content "Currency"
+      end
+    end
+
   end
 end
