46a6f230094ca3fe969cd41dde0d849ae39a937d
Peter Berkenbosch <peter@pero-ict.nl>
2014-01-28 22:50:45 +0100

    make sure we don't render the currency selector when currency change is not allowed.

--- spec/features/product_prices_spec.rb
+++ spec/features/product_prices_spec.rb
@@ -33,11 +33,27 @@ describe "Product with prices in multiple currencies" do
         end
       end
 
-      it "will not render the currency select" do
+      it "will not render the currency selector" do
         visit spree.product_path(product)
         expect(page).to_not have_content "Currency"
       end
     end
 
+    context "and allow_currency_change is false" do
+      context "and show_currency_selector is true" do
+        before do
+          reset_spree_preferences do |config|
+            config.supported_currencies = "USD,EUR,GBP"
+            config.allow_currency_change = false
+            config.show_currency_selector = true
+          end
+        end
+
+        it "will not render the currency selector" do
+          visit spree.product_path(product)
+          expect(page).to_not have_content "Currency"
+        end
+      end
+    end
   end
 end
