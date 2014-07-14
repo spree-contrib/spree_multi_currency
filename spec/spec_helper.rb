c1fb86a0183d198df677c65973f4b664707dba5f
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-07-14 11:33:00 -0400

    Bump version.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -56,6 +56,7 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
   config.color = true
+  config.infer_spec_type_from_file_location
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
