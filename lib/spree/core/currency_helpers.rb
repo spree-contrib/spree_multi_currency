04691aa64d915e76d52f2ef589fc1c972fc3ecb3
Josua Schmid <jschmid@fastmail.net>
2014-12-08 15:59:26 +0100

    Strip Currency Iso Codes
    Comma separated lists should allow whitespaces.

--- lib/spree/core/currency_helpers.rb
+++ lib/spree/core/currency_helpers.rb
@@ -5,7 +5,7 @@ module Spree
     end
 
     def supported_currencies
-      Spree::Config[:supported_currencies].split(',').map { |code| ::Money::Currency.find(code) }
+      Spree::Config[:supported_currencies].split(',').map { |code| ::Money::Currency.find(code.strip) }
     end
   end
 end
