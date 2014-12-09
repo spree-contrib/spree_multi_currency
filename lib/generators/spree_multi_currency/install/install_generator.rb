cf4b3d41dda7c197caaf73f1ff4a9efe0a578621
Tobias Bohwalli <hi@futhr.io>
2014-12-10 00:10:28 +0100

    No need to inject empty manifest css.

--- lib/generators/spree_multi_currency/install/install_generator.rb
+++ lib/generators/spree_multi_currency/install/install_generator.rb
@@ -1,17 +1,11 @@
 module SpreeMultiCurrency
   module Generators
     class InstallGenerator < Rails::Generators::Base
-
-      class_option :auto_run_migrations, :type => :boolean, :default => false
+      class_option :auto_run_migrations, type: :boolean, default: false
 
       def add_javascripts
         append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_multi_currency\n"
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_multi_currency\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_multi_currency\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_multi_currency\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
