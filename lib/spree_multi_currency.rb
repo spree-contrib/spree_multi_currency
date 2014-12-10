6d560b678da086deeb100b4b0feefd54b124d011
Tobias Bohwalli <hi@futhr.io>
2014-12-10 01:22:10 +0100

    Build version with Gem::Version.new

--- lib/spree_multi_currency.rb
+++ lib/spree_multi_currency.rb
@@ -1,3 +1,4 @@
 require 'coffee_script'
 require 'spree_core'
 require 'spree_multi_currency/engine'
+require 'spree_multi_currency/version'
---
+++ lib/spree_multi_currency/version.rb
@@ -0,0 +1,18 @@
+module SpreeMultiCurrency
+  module_function
+
+  # Returns the version of the currently loaded SpreeMultiCurrency as a
+  # <tt>Gem::Version</tt>.
+  def version
+    Gem::Version.new VERSION::STRING
+  end
+
+  module VERSION
+    MAJOR = 2
+    MINOR = 0
+    TINY  = 0
+    PRE   = nil
+
+    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
+  end
+end
