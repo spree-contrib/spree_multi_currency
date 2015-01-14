067a3a13336b9d8d9ee0c8bd5480ac1b98cb996e
Tobias Bohwalli <hi@futhr.io>
2015-01-14 13:37:00 +0100

    Make Rubocop happy with consistent syntax.

--- app/controllers/spree/admin/general_settings_controller_decorator.rb
+++ app/controllers/spree/admin/general_settings_controller_decorator.rb
@@ -1,20 +1,24 @@
-Spree::Admin::GeneralSettingsController.class_eval do
-  before_filter :update_currency_settings, only: :update
+module Spree
+  module Admin
+    GeneralSettingsController.class_eval do
+      before_filter :update_currency_settings, only: :update
 
-  def render *args
-    @preferences_currency |= [:allow_currency_change, :show_currency_selector, :supported_currencies]
-    super
-  end
-
-  private
-
-    def update_currency_settings
-      params.each do |name, value|
-        next unless Spree::Config.has_preference? name
-        if name == "supported_currencies"
-          value = value.split(',').map { |curr| ::Money::Currency.find(curr.strip).try(:iso_code) }.concat([Spree::Config[:currency]]).uniq.compact.join(',')
-        end
-        Spree::Config[name] = value
+      def render(*args)
+        @preferences_currency |= [:allow_currency_change, :show_currency_selector, :supported_currencies]
+        super
       end
+
+      private
+
+      def update_currency_settings
+        params.each do |name, value|
+          next unless Spree::Config.has_preference? name
+          if name == 'supported_currencies'
+            value = value.split(',').map { |curr| ::Money::Currency.find(curr.strip).try(:iso_code) }.concat([Spree::Config[:currency]]).uniq.compact.join(',')
+          end
+          Spree::Config[name] = value
+        end
+      end
+    end
   end
 end
--- spec/features/product_prices_spec.rb
+++ spec/features/product_prices_spec.rb
@@ -1,13 +1,12 @@
 # coding: utf-8
 
-feature "Product with prices in multiple currencies", :js do
-
-  context "with USD, EUR and GBP as currencies" do
+feature 'Product with prices in multiple currencies', :js do
+  context 'with USD, EUR and GBP as currencies' do
     let!(:product) { create(:product) }
 
     background do
       reset_spree_preferences do |config|
-        config.supported_currencies = "USD,EUR,GBP"
+        config.supported_currencies = 'USD,EUR,GBP'
         config.allow_currency_change = true
         config.show_currency_selector = true
       end
@@ -15,43 +14,43 @@ feature "Product with prices in multiple currencies", :js do
       create(:price, variant: product.master, currency: 'GBP', amount: 23.00)
     end
 
-    scenario "can switch by currency" do
+    scenario 'can switch by currency' do
       visit spree.product_path(product)
-      expect(page).to have_content("$19.99")
-      select "EUR",from: "currency"
-      expect(page).to have_content("€16.00")
-      select "GBP",from: "currency"
-      expect(page).to have_content("£23.00")
+      expect(page).to have_content('$19.99')
+      select 'EUR', from: 'currency'
+      expect(page).to have_content('€16.00')
+      select 'GBP', from: 'currency'
+      expect(page).to have_content('£23.00')
     end
 
-    context "and show_currency_selector is false" do
+    context 'and show_currency_selector is false' do
       background do
         reset_spree_preferences do |config|
-          config.supported_currencies = "USD,EUR,GBP"
+          config.supported_currencies = 'USD,EUR,GBP'
           config.allow_currency_change = true
           config.show_currency_selector = false
         end
       end
 
-      scenario "will not render the currency selector" do
+      scenario 'will not render the currency selector' do
         visit spree.product_path(product)
-        expect(page).to_not have_content "Currency"
+        expect(page).to_not have_content 'Currency'
       end
     end
 
-    context "and allow_currency_change is false" do
-      context "and show_currency_selector is true" do
+    context 'and allow_currency_change is false' do
+      context 'and show_currency_selector is true' do
         background do
           reset_spree_preferences do |config|
-            config.supported_currencies = "USD,EUR,GBP"
+            config.supported_currencies = 'USD,EUR,GBP'
             config.allow_currency_change = false
             config.show_currency_selector = true
           end
         end
 
-        scenario "will not render the currency selector" do
+        scenario 'will not render the currency selector' do
           visit spree.product_path(product)
-          expect(page).to_not have_content "Currency"
+          expect(page).to_not have_content 'Currency'
         end
       end
     end
