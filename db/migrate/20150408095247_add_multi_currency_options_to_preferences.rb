37099a8967a94abbfcd84ab18dd2b7c08939e6c2
Raul Popadineti <raulpopadineti@gmail.com>
2015-04-09 12:58:40 +0300

    revert(): remove unnecessary migration

--- db/migrate/20150408095247_add_multi_currency_options_to_preferences.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from spree_i18n (originally 20150224152415)
-class AddMultiCurrencyOptionsToPreferences < ActiveRecord::Migration
-  def change
-    add_column :spree_preferences, :allow_currency_change, :boolean
-    add_column :spree_preferences, :show_currency_selector, :boolean
-    add_column :spree_preferences, :supported_currencies, :string
-  end
-end
