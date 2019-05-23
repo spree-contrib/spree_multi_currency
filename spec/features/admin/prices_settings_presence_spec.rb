953601e30bcf97396ff33e6e90809b2feefb78d3
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-05-23 10:39:07 +0200

    Changes prices_settings_presence_spec.rb and order_prices_spec.rb

--- spec/features/admin/prices_settings_presence_spec.rb
+++ spec/features/admin/prices_settings_presence_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 feature 'Currencies settings presence', js: true do
   stub_authorization!
 
--- spec/features/order_prices_spec.rb
+++ spec/features/order_prices_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe 'Order', :js do
   given!(:product) { create(:product) }
 
