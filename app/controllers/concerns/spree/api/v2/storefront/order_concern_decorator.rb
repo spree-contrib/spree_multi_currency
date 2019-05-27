853480f137904f6b15d352fac3f56135e0310ee6
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-05-27 12:36:19 +0200

    Updates order_concern_decorator.rb

--- app/controllers/concerns/spree/api/v2/storefront/order_concern_decorator.rb
+++ app/controllers/concerns/spree/api/v2/storefront/order_concern_decorator.rb
@@ -1,31 +1,38 @@
 # Replaces currency in find_spree_current_order with actualy used currency
 # frozen_string_literal: true
+module Spree
+  module Api
+    module V2
+      module Storefront
+        module OrderConcernDecorator
+          private
 
-module OrderConcernDecorator
-  private
+          def find_spree_current_order
+            Spree::Api::Dependencies.storefront_current_order_finder.constantize.new.execute(
+              store: spree_current_store,
+              user: spree_current_user,
+              token: order_token,
+              currency: use_current_currency
+            )
+          end
 
-  def find_spree_current_order
-    Spree::Api::Dependencies.storefront_current_order_finder.constantize.new.execute(
-      store: spree_current_store,
-      user: spree_current_user,
-      token: order_token,
-      currency: use_current_currency
-    )
-  end
+          def use_current_currency
+            if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
+              session[:currency]
+            else
+              Spree::Config[:currency]
+            end
+          end
 
-  def use_current_currency
-    if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
-      session[:currency]
-    else
-      Spree::Config[:currency]
-    end
-  end
-
-  def supported_currencies
-    Spree::Config[:supported_currencies].split(',').map do |code|
-      ::Money::Currency.find(code.strip)
+          def supported_currencies
+            Spree::Config[:supported_currencies].split(',').map do |code|
+              ::Money::Currency.find(code.strip)
+            end
+          end
+        end
+      end
     end
   end
 end
 
-Spree::Api::V2::Storefront::OrderConcern.prepend OrderConcernDecorator
+Spree::Api::V2::Storefront::OrderConcern.prepend Spree::Api::V2::Storefront::OrderConcernDecorator
