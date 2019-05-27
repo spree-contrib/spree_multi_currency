24de45d1076049b0910ca27eb0674641cb4991a6
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-05-27 11:44:29 +0200

    Updates capybara.rb and Gemfile

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,7 +5,7 @@ require 'selenium-webdriver'
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new app,
     browser: :chrome,
-    options: Selenium::WebDriver::Chrome::Options.new(args: %w[disable-popup-blocking  disable-gpu window-size=1920,1080])
+    options: Selenium::WebDriver::Chrome::Options.new(args: %w[disable-popup-blocking headless disable-gpu window-size=1920,1080])
 end
 
 Capybara.javascript_driver = :chrome
