b5933b5889365c33b40c0f7ac8f6a95bf05f5722
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2020-03-10 13:37:06 +0100

    Corrects admin product tabs override and create price link partial

--- app/overrides/spree/admin/shared/_product_tabs/add_prices_to_product_tabs.rb
+++ app/overrides/spree/admin/shared/_product_tabs/add_prices_to_product_tabs.rb
@@ -3,11 +3,6 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_product_tabs',
   name: 'add-prices-to-product-tabs',
-  insert_bottom: 'ul[data-hook="admin_product_tabs"]',
-  text: <<-HTML
-          <li class="<%= 'active' if current == 'Prices' %>">
-            <% title = Spree.t(:prices) %>
-            <%= link_to_with_icon 'money', title, admin_product_prices_path(@product), title: title %>
-          </li>
-        HTML
+  insert_bottom: '[data-hook="admin_product_tabs"]',
+  partial: 'spree/admin/prices/prices_link'
 )
